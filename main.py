866e96c28657a2b23ca4cbcd65b353f1fa22c4e8
MihanEGGS <125635739+MihanEGGS@users.noreply.github.com>
2024-02-14 13:00:21 +0200

    Ready main.py
    mmelkov

--- main.py
+++ main.py
@@ -15,22 +15,61 @@
 # - iespēja apskatīt vidējo izdevumu summu
 # [!] Programmai jaglabā izdevumu stāvokli kad programma ir izslēgta palaista no jauna
 #
-
+import json
+a = 0
 expenses = []
-
+TotalSum = 0
 # load expenses from expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Python read JSON file)
-pass
-
+expenses_file = open('expenses.json') # opening JSON file
+expenses = json.load(expenses_file) # returns JSON object as a dictionary
+expenses_file.close()
 while True:
+    print("1. ievadīt izdevumus: nosaukumu, summu un kategoriju ")
+    print("2. atspoguļot uz ekrāna visus izdevumus ")
+    print("3. iespēja atlasīt 10 lielākus izdevumus ")
+    print("4. iespēja atlasīt 10 mazākus izdevumus ")
+    print("5. iespēja apskatīt vidējo izdevumu summu ")
+    print("e. Exit ")
     command = input("\nChoose command:")
     if command == "1":
-        pass
+        Name = input ("Ievadiet nosaukumu ")
+        Summ = int(input ("Ievadiet summu "))
+        Kategorie = input("Ievadiet kategoriju ") 
+        category = {
+            "Name" : Name,
+            "Sum" : Summ,
+            "Kategorie" : Kategorie
+
+        }
+        expenses.append(category)
+    elif command == "2":
+        print(expenses)
+    elif command == "3":
+        def sortede(expenses):
+            return int(expenses["Sum"])
+        neu = sorted(expenses, key =sortede, reverse = True)
+        for x in neu[:10]:
+            print(x)
+    elif command == "4":
+        def sortede(expenses):
+            return int(expenses["Sum"])
+        neu = sorted(expenses, key =sortede)
+        for x in neu[:10]:
+            print(x)
+    elif command == "5":
+        for i in expenses:
+            a += 1
+            TotalSum = TotalSum + i["Sum"]
+        AverageRaw = TotalSum/a
+        Average = round(AverageRaw, 2)
+        print("Average sum is ", Average)
     if command == "e":
         print("Exiting...")
         break
 
 # save expenses to expenses.json file here
 # https://www.geeksforgeeks.org/read-write-and-parse-json-using-python/ (Writing JSON to a file in Python)
-pass
+with open("expenses.json", "w") as file: 
+    json.dump(expenses, file)
 
